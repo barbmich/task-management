3d9db3e4114761e5663d9383ea3d39a52c917a03
Michele Barbiero <barbmich.since86@gmail.com>
2021-01-11 15:12:28 -0500

    add tasks/reset endpoint to delete all rows in task table to ease testing

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -22,8 +22,17 @@ import { TasksService } from './tasks.service';
 export class TasksController {
   constructor(private tasksService: TasksService) {}
 
+  @Get('/reset')
+  deleteAllTasks(
+    @Query(ValidationPipe) filterDto: GetTasksFilterDto,
+  ): Promise<void> {
+    return this.tasksService.deleteAllTasks(filterDto);
+  }
+
   @Get()
-  getTasks(@Query(ValidationPipe) filterDto: GetTasksFilterDto) {
+  getTasks(
+    @Query(ValidationPipe) filterDto: GetTasksFilterDto,
+  ): Promise<Task[]> {
     return this.tasksService.getTasks(filterDto);
   }
 
