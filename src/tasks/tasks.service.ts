ce8c9b3704597e6ca87ac66b638ec2682445ec7b
Michele Barbiero <barbmich.since86@gmail.com>
2021-01-11 00:08:06 -0500

    add getTaskById and delete endpoints

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v4 as uuid } from 'uuid';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Injectable()
 export class TasksService {
@@ -10,15 +11,26 @@ export class TasksService {
     return this.tasks;
   }
 
-  createTask(title: string, description: string): Task {
+  getTaskById(id: string): Task {
+    return this.tasks.find((task) => task.id === id);
+  }
+
+  createTask(createTaskDto: CreateTaskDto): Task {
+    const { title, description } = createTaskDto;
     const task: Task = {
       id: uuid(),
-      title: title,
-      description: description,
+      title,
+      description,
       status: TaskStatus.OPEN,
     };
 
     this.tasks.push(task);
     return task;
+  }
+
+  deleteTask(id: string): void {
+    this.tasks = this.tasks.filter((task) => {
+      return task.id !== id;
+    });
   }
 }
